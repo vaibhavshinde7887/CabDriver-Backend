7460ac904ee33fc950bd5c56df2478ed03ea099c
vaibhavshinde7887 <vshinde7887@gmail.com>
2024-06-30 15:35:49 +0530

    initial code setup done

---
+++ src/main/java/com/cabdriver/Cab/Driver/CabDriverApplication.java
@@ -0,0 +1,13 @@
+package com.cabdriver.Cab.Driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabDriverApplication.class, args);
+	}
+
+}
